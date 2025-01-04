390cbd9f81d40139f2447e970d5abc839e4fe060
rktech0078 <rafaykhan007852@gmail.com>
2025-01-04 17:05:47 +0500

    <br /> tag

--- app/blogs/[details]/page.tsx
+++ app/blogs/[details]/page.tsx
@@ -79,7 +79,7 @@ export default function Page(props: { params: { details: string } }) {
                                     d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
                                 />
                             </svg>
-                            <span>Warning: Invalid blog page!</span>
+                            <span>Warning: Invalid blog page!</span> <br />
                             <span>Please try again a while</span>
                         </div>
                     )}
